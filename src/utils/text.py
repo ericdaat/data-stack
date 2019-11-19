dd226b6ace1286ffb6e36d46644aac57b635479d
Eric <daoudattoyan.eric@gmail.com>
2019-11-19 11:35:24 +0100

    add text in utils

---
+++ src/utils/text.py
@@ -0,0 +1,38 @@
+import re
+
+
+def isolate_punctuation(text):
+    """ Isolate punctuation in a sentence.
+
+    >>> split_punctuation('Hi there!')
+    'Hi there !'
+
+    Args:
+        text (str): Input sentence
+
+    Returns:
+        str: Output sentence with isolated punctuation
+    """
+    text = re.sub('([.,!?()])', r' \1 ', text)
+    text = re.sub('\s{2,}', ' ', text)
+
+    return text.strip()
+
+
+def replace_urls(text, replace_with="<URL>"):
+    """Replace urls in a sentence with a chosen string.
+
+    >>> replace_urls("I love https://github.com")
+    "I love <URL>"
+
+    Args:
+        text (str): Input sentence
+        replace_with (str, optional): string to replace the url with. Defaults to "<URL>".
+
+    Returns:
+        str: Output sentence with replaced url
+    """
+    url_regex = re.compile(r"((http|ftp|https):\/\/)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(.*)")
+    text = url_regex.sub(replace_with, r)
+
+    return text
